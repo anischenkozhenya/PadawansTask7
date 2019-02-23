bacab63180a1776dce2511857a0eb8b0d8a27fd4
Евгений Анищенко <anischenkozhenya@gmail.com>
2019-02-23 17:55:12 +0300

    add exception

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,6 +6,13 @@ namespace PadawansTask7
     {
         public static void OrderStringsByLength(string[] array)
         {
+            if (array == null)
+                throw new ArgumentNullException();
+            foreach (string a in array)
+                if (a == null)
+                    throw new ArgumentNullException();
+            if (array.Length <= 0)
+                throw new ArgumentException();
             int[] legthwordinstring = new int[array.Length];
             bool ch;
             void swap(int[] a, string[] b, int i)
